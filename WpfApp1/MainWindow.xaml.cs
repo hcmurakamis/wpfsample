34b81daa8105f600988a0778c5df07a01873ee21
murakami <dischordman@gmail.com>
2024-05-26 07:01:08 +0900

    add button with picture

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
